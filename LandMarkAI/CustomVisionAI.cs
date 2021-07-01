0f660671118088e8cfe12b09bed616298b4515d9
Silverback1992 <44880539+Silverback1992@users.noreply.github.com>
2021-07-01 19:01:27 +0200

    Initial commit

---
+++ LandMarkAI/CustomVisionAI.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace LandMarkAI
+{
+    public static class CustomVisionAI
+    {
+        private static string _url;
+        private static string _predictionKey;
+        private static string _contentType;
+
+        static CustomVisionAI()
+        {
+            _url = "https://westeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/bf9dcd84-caac-47f6-85de-c7c3e611058b/classify/iterations/LandmarkModel/image";
+            _predictionKey = "918d188543364c8bb87a8f8d9afa155e";
+            _contentType = "application/octet-stream";
+        }
+
+        public async static void CustomVisionAIPredict(MainWindow mw, string filePath)
+        {
+            var f = File.ReadAllBytes(filePath);
+
+            using(var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Prediction-Key", _predictionKey);
+
+                using(var content = new ByteArrayContent(f))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue(_contentType);
+                    var response = await client.PostAsync(_url, content);
+                    string responseString = await response.Content.ReadAsStringAsync();
+
+                    var predictions = JsonConvert.DeserializeObject<CustomVisionResponse>(responseString).Predictions;
+
+                    mw.Dispatcher.Invoke(() => { mw.predictionsListView.ItemsSource = predictions; });
+                }
+            }
+        }
+    }
+}
---
+++ LandMarkAI/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LandMarkAI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void fileBrowserButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.png; *.jpg)|*.png;*.jpg;*.jpeg|All files (*.*)|*.*"; //before the pipe its just text/info for the user
+
+            if(dialog.ShowDialog() == true)
+            {
+                selectedImagePathTextBox.Text = dialog.FileName;
+                selectedImage.Source = new BitmapImage(new Uri(dialog.FileName));
+            }
+        }
+
+        private void processImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(String.IsNullOrWhiteSpace(selectedImagePathTextBox.Text))
+            {
+                MessageBox.Show("Please select an image first.");
+            }
+            else
+            {
+                CustomVisionAI.CustomVisionAIPredict(this, selectedImagePathTextBox.Text);
+            }
+        }
+    }
+}
